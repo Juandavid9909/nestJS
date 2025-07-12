8f584c69ff0ab2cc70c48751d9ea01d68e0bb248
Juan David <juandavid918@gmail.com>
2025-07-12 12:52:37 -0500

    Bases para autenticación

---
+++ 04-teslo-shop/src/auth/auth.service.ts
@@ -0,0 +1,56 @@
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import * as bcrypt from 'bcrypt';
+import { Repository } from 'typeorm';
+
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+
+export interface UserData {
+  email: string;
+  password?: string;
+  fullName: string;
+  isActive?: boolean;
+  roles?: string[];
+}
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const { password, ...userData } = createUserDto;
+      const user: UserData = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync(password, 10),
+      });
+
+      await this.userRepository.save(user);
+
+      delete user.password;
+
+      return user;
+    } catch (error) {
+      this.handleDBErrors(error as { [key: string]: any });
+    }
+  }
+
+  private handleDBErrors(error: { [key: string]: any }): never {
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    }
+
+    console.log(error);
+
+    throw new InternalServerErrorException('Please check server logs');
+  }
+}
